f1f2a1a6bea261049856d3146b0bfe3d9e434331
Kishori M Konwar <kishori82@gmail.com>
2018-04-12 14:08:24 -0400

    added the message size to the client

--- oplatency.go
+++ oplatency.go
@@ -8,14 +8,14 @@
 package main
 
 import (
-	zmq "github.com/pebbe/zmq3"
-        "bytes"
+	"bytes"
 	"encoding/gob"
+	"flag"
 	"fmt"
+	zmq "github.com/pebbe/zmq3"
 	"log"
 	"math/rand"
 	"time"
-        "flag"
 )
 
 //  ---------------------------------------------------------------------
@@ -25,13 +25,15 @@ import (
 //  run several client tasks in parallel, each with a different random ID.
 
 const NON_BLOCKING = 1
+
 type Message struct {
-   OpType string
-   Msg_number int
-   Data []byte 
+	OpType     string
+	Msg_number int
+	Msg_size   int
+	Data       []byte
 }
 
-func client_task(receiverIP string, numMessages int, msgSize int, opType string ) {
+func client_task(receiverIP string, numMessages int, msgSize int, opType string) {
 
 	client, _ := zmq.NewSocket(zmq.DEALER)
 	defer client.Close()
@@ -40,55 +42,54 @@ func client_task(receiverIP string, numMessages int, msgSize int, opType string 
 	set_id(client)
 	client.Connect("tcp://" + receiverIP + ":5555")
 
-        poller := zmq.NewPoller()
-        poller.Add(client, zmq.POLLIN)
+	poller := zmq.NewPoller()
+	poller.Add(client, zmq.POLLIN)
 
-        request_nbr := 0
-        var message Message
+	request_nbr := 0
+	var message Message
 
-        message.OpType=opType
-        if message.OpType=="write" {
-           message.Data = make([]byte, msgSize)
-        }
-        if message.OpType=="read" {
-           message.Data = make([]byte, 0)
-        }
+	message.OpType = opType
+	if message.OpType == "write" {
+		message.Data = make([]byte, msgSize)
+	}
+	if message.OpType == "read" {
+		message.Data = make([]byte, 0)
+	}
+	message.Msg_size = msgSize
 
 	rand.Read(message.Data)
 
-        var totTime int64 = 0
-	for i:=0; i < numMessages; i++ {
+	var totTime int64 = 0
+	for i := 0; i < numMessages; i++ {
 		//time.Sleep(time.Second)
 		//client.SendMessage(fmt.Sprintf("request #%d", request_nbr))
-                start := time.Now()
-                message.Msg_number = request_nbr
-                message_to_send := CreateGobFromMessage(message)
-                _, err := client.SendBytes(message_to_send.Bytes(), NON_BLOCKING)
+		start := time.Now()
+		message.Msg_number = request_nbr
+		message_to_send := CreateGobFromMessage(message)
+		_, err := client.SendBytes(message_to_send.Bytes(), NON_BLOCKING)
 		if err != nil {
 			fmt.Println("Sending message failed ", request_nbr)
 		}
 		//fmt.Println("Sending ", message.Msg_number, len(message.Data))
 		time.Sleep(1 * time.Millisecond)
 
-                sockets, err := poller.Poll(-1) 
-                for _, socket:= range sockets {
-                   msgBytes, err := socket.Socket.RecvBytes(0)
-                   message1 := CreateMessageFromGob(msgBytes)
-		   //fmt.Println("Received ", message1.Msg_number, len(message1.Data))
-                   if err!=nil {
-		      fmt.Println("Error in receiving", len(message1.Data))
+		sockets, err := poller.Poll(-1)
+		for _, socket := range sockets {
+			msgBytes, err := socket.Socket.RecvBytes(0)
+			message1 := CreateMessageFromGob(msgBytes)
+			//fmt.Println("Received ", message1.Msg_number, len(message1.Data))
+			if err != nil {
+				fmt.Println("Error in receiving", len(message1.Data))
 
-                   }
-                }
-	        elapsed := time.Since(start)
-	        //fmt.Println("READ TIME:",  elapsed)
-                totTime += int64(elapsed)
+			}
+		}
+		elapsed := time.Since(start)
+		//fmt.Println("READ TIME:",  elapsed)
+		totTime += int64(elapsed)
 
 		request_nbr += 2
 	}
-        fmt.Println("Avg Latency " + opType, totTime/int64(1000*numMessages))
-
-
+	fmt.Println("Avg Latency "+opType, totTime/int64(1000*numMessages))
 
 }
 
@@ -110,10 +111,8 @@ func server_task(msgSize int) {
 	defer backend.Close()
 	backend.Bind("inproc://backend")
 
-	//  Launch pool of worker threads, precise number is not critical
-	for i := 0; i < 5; i++ {
-		go server_worker(msgSize)
-	}
+	//  Launch a worker threads, precise number is not critical
+	go server_worker()
 
 	//  Connect backend to frontend via a proxy
 	err := zmq.Proxy(frontend, backend, nil)
@@ -123,37 +122,44 @@ func server_task(msgSize int) {
 //  Each worker task works on one request at a time and sends a random number
 //  of replies back, with random delays between replies:
 
-func server_worker(msgSize int) {
+func server_worker() {
 
 	worker, _ := zmq.NewSocket(zmq.DEALER)
 	defer worker.Close()
 	worker.Connect("inproc://backend")
-        msg_reply := make([][]byte, 2)
-        for i := 0; i < len(msg_reply); i++ {
+	msg_reply := make([][]byte, 2)
+	for i := 0; i < len(msg_reply); i++ {
 		msg_reply[i] = make([]byte, 0) // the frist frame  specifies the identity of the sender, the second specifies the content
 	}
 
-        read_data := make([]byte, msgSize)
-	rand.Read(read_data)
+	var read_data []byte
+	var isDataInitialized bool = false
 
 	for {
-                msg, err := worker.RecvMessageBytes(0)
+		msg, err := worker.RecvMessageBytes(0)
 		if err != nil {
-	            fmt.Println("Somthing Wrong", err)
+			fmt.Println("Somthing Wrong", err)
 		}
 		message := CreateMessageFromGob(msg[1])
 
-                //sender
-                msg_reply[0] = msg[0]
+		msgSize := message.Msg_size
+		if isDataInitialized == false {
+			read_data = make([]byte, msgSize)
+			rand.Read(read_data)
+			isDataInitialized = true
+		}
 
-                //prepare response
-                message.Msg_number++
-                if message.OpType=="read" {
-                   message.Data = read_data
-                }
-                if message.OpType=="write" {
-                   message.Data = make([]byte, 0)
-                }
+		//sender
+		msg_reply[0] = msg[0]
+
+		//prepare response
+		message.Msg_number++
+		if message.OpType == "read" {
+			message.Data = read_data
+		}
+		if message.OpType == "write" {
+			message.Data = make([]byte, 0)
+		}
 
 		bytes_buffer_temp := CreateGobFromMessage(message)
 		msg_reply[1] = bytes_buffer_temp.Bytes()
@@ -168,24 +174,24 @@ func server_worker(msgSize int) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-        procType := flag.String("process_type", "both", "process type sender/receiver")
-        receiverIP:= flag.String("receiver_ip", "127.0.0.1", "recv process IP/port:12000")
-        numMessages:= flag.Int("num_messages", 1000, "number of messages")
-        msgSize:= flag.Int("size_message", 1000, "size of messages")
-        flag.Parse()
+	procType := flag.String("process_type", "both", "process type sender/receiver")
+	receiverIP := flag.String("receiver_ip", "127.0.0.1", "recv process IP/port:12000")
+	numMessages := flag.Int("num_messages", 1000, "number of messages")
+	msgSize := flag.Int("size_message", 1000, "size of messages")
+	flag.Parse()
 
-        if *procType=="both" {
-	   go server_task(*msgSize)
-	   client_task(*receiverIP, *numMessages, *msgSize,"read")
-	   client_task(*receiverIP, *numMessages, *msgSize,"write")
-        }
-        if *procType=="server" {
-	   server_task(*msgSize)
-        }
-        if *procType=="client" {
-	   client_task(*receiverIP, *numMessages, *msgSize, "read")
-	   client_task(*receiverIP, *numMessages, *msgSize, "write")
-        }
+	if *procType == "both" {
+		go server_task(*msgSize)
+		client_task(*receiverIP, *numMessages, *msgSize, "read")
+		client_task(*receiverIP, *numMessages, *msgSize, "write")
+	}
+	if *procType == "server" {
+		server_task(*msgSize)
+	}
+	if *procType == "client" {
+		client_task(*receiverIP, *numMessages, *msgSize, "read")
+		client_task(*receiverIP, *numMessages, *msgSize, "write")
+	}
 
 	//  Run for 5 seconds then quit
 	time.Sleep(1000000 * time.Second)
@@ -195,7 +201,6 @@ func set_id(soc *zmq.Socket) {
 	identity := fmt.Sprintf("%04X-%04X", rand.Intn(0x10000), rand.Intn(0x10000))
 	soc.SetIdentity(identity)
 }
-
 
 func CreateGobFromMessage(message Message) bytes.Buffer {
 
